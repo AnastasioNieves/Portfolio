852da983fd6e59b33afccbc2136f6baaaf0c801d
Anastasio Nieves <tasioo23@gmail.com>
2026-06-20 21:16:03 +0200

    Remove Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
         <Hero />
         <Skills />
         <Projects />
-        <Contact />
       </main>
       <Footer />
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,6 @@ const Header = () => {
             <li><a href="#about">Sobre mí</a></li>
             <li><a href="#skills">Habilidades</a></li>
             <li><a href="#projects">Proyectos</a></li>
-            <li><a href="#contact">Contacto</a></li>
           </ul>
         </nav>
       </div>
